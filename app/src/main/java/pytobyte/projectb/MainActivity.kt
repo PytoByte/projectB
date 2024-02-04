809f0f8af8bf0aea42a1dda5b37674243136e995
PytoByte <78215689+PytoByte@users.noreply.github.com>
2024-02-04 19:17:30 +0700

    FINAL_1

--- app/src/main/java/pytobyte/projectb/MainActivity.kt
+++ app/src/main/java/pytobyte/projectb/MainActivity.kt
@@ -65,6 +65,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -134,10 +135,11 @@ fun Screen1(screenState: MutableState<String>) {
             contentAlignment = Alignment.Center
         ) {
             Text(
-                text = "Демонстрация анимации",
+                text = "Демонстрация анимации\n(На примере ValorantAPI)",
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black,
+                textAlign = TextAlign.Center
             )
         }
     }
